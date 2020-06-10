2ac7759e4a4e8c23a453cb2561b2329828bc9dc0
tom <tom@sapletta.com>
2020-06-10 15:54:30 +0200

    better version

---
+++ js/loader.js
@@ -0,0 +1,33 @@
+// create.js
+
+// document.onload = function () {
+//     document.getElementById("myForm").addEventListener("submit", function (event) {
+//         event.preventDefault();
+//         loader();
+//         console.log("form is submitted");
+//     });
+// }
+
+function loader(event) {
+    event.preventDefault();
+
+    var x = document.forms["myForm"]["json"].value;
+    if (x == "") {
+        alert("Field JSON must be filled out");
+        return false;
+    }
+
+    var script = document.createElement('script');
+    // script.src = (window.location.hostname === 'localhost') ? "//localhost:8080/load.js" : "//load.jloads.com/load.js";
+    // script.src = "//load.jloads.com/load.js";
+    var b64 = btoa(x);
+    console.log(b64);
+
+    script.src = "//get.wapka.pl/b64/" + b64;
+    // script.src = "//localhost:80/b64/" + b64;
+    document.head.appendChild(script);
+
+    document.forms["myForm"]["url"].value = script.src;
+
+    return false;
+}
