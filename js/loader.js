3f94417ff1be5f0393f56498b13b7cc9a5d99cf9
tom <tom@sapletta.com>
2020-06-11 18:29:01 +0200

    better version

--- js/loader.js
+++ js/loader.js
@@ -2,6 +2,8 @@
 
 waitForElementToDisplay('#submit', 200, function (i){
     var elem = document.querySelectorAll(i)[0] || document.querySelectorAll(i);
+    console.log('waitForElementToDisplay elem', elem, elem["json"]);
+
     elem.addEventListener("click", function (event) {
         event.preventDefault();
         loader(elem);
