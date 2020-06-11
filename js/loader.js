152536fbc21da6b9b577d429ceaaa736a062792f
tom <tom@sapletta.com>
2020-06-11 20:22:35 +0200

    better version

--- js/loader.js
+++ js/loader.js
@@ -1,27 +1,27 @@
 // loader.js
-log('exist?','loader.waitForElementToDisplay');
-waitForElementToDisplay('#submit', 200, function (i){
+jlogs('exist?','loader.waitForElementToDisplay');
+waitForElementToDisplay('#create', 200, function (i){
 
-    console.log('waitForElementToDisplay i', i);
+    jlogs('waitForElementToDisplay i', i);
 
     var elem = document.querySelectorAll(i)[0] || document.querySelectorAll(i);
-    console.log('waitForElementToDisplay elem', elem, elem["json"]);
+    jlogs('waitForElementToDisplay elem', elem, elem["json"]);
 
-    elem.addEventListener("click", function (event) {
+    elem.addEventListener("submit", function (event) {
         event.preventDefault();
         loader(elem);
-        console.log("form is submitted");
+        jlogs("form is submitted");
     });
 });
 
-log('exist?','loader');
+jlogs('exist?','loader');
 /**
  * @param elem
  * @returns {boolean}
  */
 function loader(elem) {
 
-    console.log('loader elem', elem, elem["json"]);
+    jlogs('loader elem', elem, elem["json"]);
 
     var x = elem["json"].value;
     if (x == "") {
@@ -33,7 +33,7 @@ function loader(elem) {
     // script.src = (window.location.hostname === 'localhost') ? "//localhost:8080/load.js" : "//load.jloads.com/load.js";
     // script.src = "//load.jloads.com/load.js";
     var b64 = btoa(x);
-    console.log(b64);
+    jlogs(b64);
 
     script.src = "//get.wapka.pl/b64/" + b64;
     // script.src = "//localhost:80/b64/" + b64;
